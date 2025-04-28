22772ae597cd5e179ae92f8b0e4f25e97cc7bb0d
nik_voi <tivkaipivka@gmial.com>
2025-04-28 16:19:14 +0300

    fix: meta data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,78 +1,16 @@
-'use client'
+import type { Metadata } from 'next'
+import ClientLayout from './client-layout'
 
-import Loader from '@/shared/ui/loader'
-import Footer from '@/widgets/footer/ui/footer'
-import Header from '@/widgets/header/ui/header'
-import { AnimatePresence, motion } from 'framer-motion'
-import { Archivo_Black, Bebas_Neue, Montserrat } from 'next/font/google'
-import { useEffect, useState } from 'react'
-import './globals.css'
-
-const montserrat = Montserrat({
-	subsets: ['latin'],
-	weight: ['400', '700'],
-	variable: '--font-montserrat',
-})
-const bebasNeue = Bebas_Neue({
-	subsets: ['latin'],
-	weight: ['400'],
-	variable: '--font-bebas',
-})
-
-const archivoBlack = Archivo_Black({
-	subsets: ['latin'],
-	weight: ['400'],
-	variable: '--font-archivo',
-})
+export const metadata: Metadata = {
+	title: 'Matush',
+	description:
+		'Matyush — бренд одежды, сочетающий премиальные материалы, современный дизайн и безупречный комфорт. Худи, пижамы, сумки и коллекции для стильных и уверенных в себе. Эстетика в каждой детали.',
+}
 
 export default function RootLayout({
 	children,
-}: Readonly<{
+}: {
 	children: React.ReactNode
-}>) {
-	const [isLoading, setIsLoading] = useState(true)
-
-	useEffect(() => {
-		const timeout = setTimeout(() => {
-			setIsLoading(false)
-		}, 1000)
-
-		return () => clearTimeout(timeout)
-	}, [])
-
-	return (
-		<html lang='ru'>
-			<body
-				className={`${montserrat.variable} ${archivoBlack.variable}  ${bebasNeue.variable} flex flex-col justify-between min-h-screen`}
-			>
-				<AnimatePresence mode='wait'>
-					{isLoading ? (
-						<motion.div
-							key='loader'
-							className='flex items-center justify-center w-full h-screen bg-white'
-							initial={{ opacity: 1 }}
-							animate={{ opacity: 1 }}
-							exit={{ opacity: 0 }}
-							transition={{ duration: 0.8 }}
-						>
-							<Loader />
-						</motion.div>
-					) : (
-						<motion.div
-							key='content'
-							initial={{ opacity: 0 }}
-							animate={{ opacity: 1 }}
-							exit={{ opacity: 0 }}
-							transition={{ duration: 0.8 }}
-							className='flex flex-col min-h-screen'
-						>
-							<Header />
-							<main className='flex-1'>{children}</main>
-							<Footer />
-						</motion.div>
-					)}
-				</AnimatePresence>
-			</body>
-		</html>
-	)
+}) {
+	return <ClientLayout>{children}</ClientLayout>
 }
